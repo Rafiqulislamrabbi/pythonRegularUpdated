c0960b0bf764293966bd3e6fa3cb2e2d91aa60b7
ubuntu <rafiqul15-2546@diu.edu.bd>
2024-05-10 21:01:11 +0600

    Initial commit

---
+++ list.py
@@ -0,0 +1,80 @@
+# my_list = [1, 2, 3, 4, 5]
+# print(len(my_list))
+
+
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+
+
+# my_list = [1, 2, 3]
+# my_list.append(4)
+# print(my_list)
+
+
+
+# my_list = [1, 2, 3]
+# my_list.extend([4, 5, 6])
+# print(my_list)
+
+
+
+# my_list = [1, 2, 3]
+# my_list.insert(1, 10)
+# print(my_list)
+
+
+
+# my_list = [1, 2, 3, 4, 3]
+# my_list.remove(3)
+# print(my_list)
+
+
+
+# my_list = [1, 2, 3, 4, 5]
+# popped_element = my_list.pop(0)
+# print(popped_element)
+# print(my_list)
+#
+#
+#
+# my_list = [1, 2, 3, 4, 5]
+# index = my_list.index(3)
+# print(index)
+
+#
+#
+# my_list = [1, 2, 3, 4, 3, 5]
+# count = my_list.count(3)
+# print(count)
+
+#
+#
+# my_list = [1, 2, 3, 4, 5]
+# my_list.reverse()
+# print(my_list)
+
+#
+#
+# my_list = [3, 1, 4, 2, 5]
+# my_list.sort()
+# print(my_list)
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+#
+#
+#
+#
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
---
+++ operators.py
@@ -0,0 +1,102 @@
+# # Arithmetic Operators
+#
+# x = 5
+# y = 3
+# print(x + y)
+#
+#
+#
+# x = 5
+# y = 3
+# print(x - y)
+#
+#
+# x = 5
+# y = 3
+# print(x * y)
+#
+#
+#
+# x = 9
+# y = 3
+# print(x / y)
+#
+#
+#
+# x = 9
+# y = 3
+# print(x // y)
+#
+#
+#
+# x = 3
+# y = 9
+# print(x % y)
+#
+#
+# x = 2
+# y = 5
+#
+# print(x ** y)
+#
+#
+#
+# # Assignment Operators
+#
+# x = 5
+# x += 3
+# print(x)
+#
+#
+#
+# # Comparison Operators
+#
+# x = 5
+# y = 3
+# print(x >= y)
+#
+#
+#
+# # Logical Operators
+#
+# x = 5
+# print(x > 3 and x < 10)
+#
+#
+#
+# x = 5
+# print(x > 3 or x < 4)
+#
+#
+#
+# x = 5
+# print(not(x > 3 and x < 10))
+#
+#
+#
+# # Identity Operators
+#
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+# print(x is z)
+#
+#
+#
+#
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+# print(x is not z)
+#
+#
+#
+# # Membership Operators
+#
+# x = ["apple", "banana"]
+# print("banana" in x)
+#
+#
+#
+# x = ["apple", "banana"]
+# print("pineapple" not in x)
